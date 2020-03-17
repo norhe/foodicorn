bdf40fe412a2c366d96057f3d51eb35af73755ab
Ehron <ehron@ehron.org>
2020-03-17 19:58:10 -0400

    slots

--- main.tf
+++ main.tf
@@ -1,3 +1,12 @@
+provider "azurerm" {
+  version = "2.1.0"
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
+    }
+  }
+}
+
 data "terraform_remote_state" "base_env" {
   backend = "remote"
 
@@ -9,34 +18,27 @@ data "terraform_remote_state" "base_env" {
   }
 }
 
-provider "azurerm" {
-  version = "2.1.0"
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy = true
+resource "azurerm_app_service_plan" "slotDemo" {
+    name                = "slotAppServicePlan"
+    location            = data.terraform_remote_state.base_env.outputs.rg-location
+    resource_group_name = data.terraform_remote_state.base_env.outputs.rg-name
+    sku {
+        tier = "Standard"
+        size = "S1"
     }
-  }
 }
 
-module "linuxservers" {
-  source              = "Azure/compute/azurerm"
-  version             = "3.0.0"
-  resource_group_name = data.terraform_remote_state.base_env.outputs.rg-name
-  vm_os_simple        = "UbuntuServer"
-  public_ip_dns       = ["linsimplevmips"] // change to a unique name per datacenter region
-  vnet_subnet_id      = module.network.vnet_subnets[0]
-  enable_ssh_key      = false
-  ssh_key             = null
+resource "azurerm_app_service" "slotDemo" {
+    name                = "slotAppService"
+    location            = data.terraform_remote_state.base_env.outputs.rg-location
+    resource_group_name = data.terraform_remote_state.base_env.outputs.rg-name
+    app_service_plan_id = azurerm_app_service_plan.slotDemo.id
 }
 
-module "network" {
-  source              = "Azure/network/azurerm"
-  version             = "3.0.0"
-  resource_group_name = data.terraform_remote_state.base_env.outputs.rg-name
-  location            = data.terraform_remote_state.base_env.outputs.rg-location
-  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24"]
-}
-
-output "linux_vm_public_name" {
-  value = module.linuxservers.public_ip_dns_name
+resource "azurerm_app_service_slot" "slotDemo" {
+    name                = "slotAppServiceSlotOne"
+    location            = data.terraform_remote_state.base_env.outputs.rg-location
+    resource_group_name = data.terraform_remote_state.base_env.outputs.rg-location
+    app_service_plan_id = azurerm_app_service_plan.slotDemo.id
+    app_service_name    = azurerm_app_service.slotDemo.name
 }
