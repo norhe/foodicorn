a9f3f9100666fd02a38915fea1be951d670792b4
Ehron <ehron@ehron.org>
2020-03-17 18:14:27 -0400

    backend

--- main.tf
+++ main.tf
@@ -21,7 +21,7 @@ provider "azurerm" {
 module "linuxservers" {
   source              = "Azure/compute/azurerm"
   version             = "3.0.0"
-  resource_group_name = data.terraform_remote_state.base_env.name
+  resource_group_name = data.terraform_remote_state.base_env.outputs.name
   vm_os_simple        = "UbuntuServer"
   public_ip_dns       = ["linsimplevmips"] // change to a unique name per datacenter region
   vnet_subnet_id      = module.network.vnet_subnets[0]
@@ -30,8 +30,8 @@ module "linuxservers" {
 module "network" {
   source              = "Azure/network/azurerm"
   version             = "2.0.0"
-  resource_group_name = data.terraform_remote_state.base_env.name
-  location            = data.terraform_remote_state.base_env.location
+  resource_group_name = data.terraform_remote_state.base_env.outputs.name
+  location            = data.terraform_remote_state.base_env.outputs.location
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
