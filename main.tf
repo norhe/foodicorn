bdf9ca54e565dccbe72befbeee1b544c093b2ebf
Ehron <ehron@ehron.org>
2020-03-17 18:02:33 -0400

    update module version

--- main.tf
+++ main.tf
@@ -17,6 +17,7 @@ provider "azurerm" {
 
 module "linuxservers" {
   source              = "Azure/compute/azurerm"
+  version             = "3.0.0"
   resource_group_name = data.terraform_remote_state.base_env.name
   vm_os_simple        = "UbuntuServer"
   public_ip_dns       = ["linsimplevmips"] // change to a unique name per datacenter region
@@ -25,7 +26,7 @@ module "linuxservers" {
 
 module "network" {
   source              = "Azure/network/azurerm"
-  #version             = "3.0.0"
+  version             = "2.0.0"
   resource_group_name = data.terraform_remote_state.base_env.name
   allow_ssh_traffic   = "true"
   subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24"]
