cc4bab9368625a30ec23d4e70556cd88879e9adc
Ehron <ehron@ehron.org>
2020-03-17 20:39:52 -0400

    use private module

--- main.tf
+++ main.tf
@@ -1,12 +1,3 @@
-provider "azurerm" {
-  version = "2.1.0"
-  features {
-    key_vault {
-      purge_soft_delete_on_destroy = true
-    }
-  }
-}
-
 data "terraform_remote_state" "base_env" {
   backend = "remote"
 
@@ -18,33 +9,34 @@ data "terraform_remote_state" "base_env" {
   }
 }
 
-resource "random_string" "random" {
-  length = 6
-  special = false
-  upper = false
-}
-
-resource "azurerm_app_service_plan" "slotDemo" {
-    name                = "${random_string.random.result}-slotAppServicePlan"
-    location            = data.terraform_remote_state.base_env.outputs.rg-location
-    resource_group_name = data.terraform_remote_state.base_env.outputs.rg-name
-    sku {
-        tier = "Standard"
-        size = "S1"
+provider "azurerm" {
+  version = "2.1.0"
+  features {
+    key_vault {
+      purge_soft_delete_on_destroy = true
     }
+  }
 }
 
-resource "azurerm_app_service" "slotDemo" {
-    name                = "${random_string.random.result}-slotAppService"
-    location            = data.terraform_remote_state.base_env.outputs.rg-location
-    resource_group_name = data.terraform_remote_state.base_env.outputs.rg-name
-    app_service_plan_id = azurerm_app_service_plan.slotDemo.id
+module "linuxservers" {
+  source              = "Azure/compute/azurerm"
+  version             = "3.0.0"
+  resource_group_name = data.terraform_remote_state.base_env.outputs.rg-name
+  vm_os_simple        = "UbuntuServer"
+  public_ip_dns       = ["linsimplevmips"] // change to a unique name per datacenter region
+  vnet_subnet_id      = module.network.vnet_subnets[0]
+  enable_ssh_key      = false
+  ssh_key             = null
 }
 
-resource "azurerm_app_service_slot" "slotDemo" {
-    name                = "${random_string.random.result}-slotAppServiceSlotOne"
-    location            = data.terraform_remote_state.base_env.outputs.rg-location
-    resource_group_name = data.terraform_remote_state.base_env.outputs.rg-location
-    app_service_plan_id = azurerm_app_service_plan.slotDemo.id
-    app_service_name    = azurerm_app_service.slotDemo.name
+module "network" {
+  source              = "app.terraform.io/synaptic_racing/network/azure"
+  version             = "3.0.0"
+  resource_group_name = data.terraform_remote_state.base_env.outputs.rg-name
+  location            = data.terraform_remote_state.base_env.outputs.rg-location
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+output "linux_vm_public_name" {
+  value = module.linuxservers.public_ip_dns_name
 }
